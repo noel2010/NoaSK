f74f4047e9b1cfe92e91457fde96c5fdd4855ca1
Noel <playgoodforgood@gmail.com>
2024-12-09 20:58:43 +0100

    Initial Commit

---
+++ src/main/java/com/noa/development/noaSK/NoaSK.java
@@ -0,0 +1,17 @@
+package com.noa.development.noaSK;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoaSK extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
